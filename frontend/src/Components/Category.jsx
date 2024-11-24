b7ea81288490a87e3d287c89fa820731dbebddc5
Hasibul-Islam <hmhasibul48@gmail.com>
2024-11-24 21:59:56 +0600

    simple fix

--- frontend/src/Components/Category.jsx
+++ frontend/src/Components/Category.jsx
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom'
 function Category({ categoryName, path, imgURL }) {
     return (
         <>
-            <div className='border p-3 text-center hover:shadow-lg transition-shadow'>
+            <div className='border p-3 text-center hover:shadow-lg group transition-shadow'>
                 <Link to={path}>
-                    <img className='w-40 h-40 object-cover rounded-full mb-3' src={imgURL} alt="Image Not Found" />
+                    <img className='w-40 h-40 object-cover rounded-full mb-3 group-hover:scale-105 transition-all' src={imgURL} alt="Image Not Found" />
                     <h4>{categoryName}</h4>
                 </Link>
             </div>
